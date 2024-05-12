8c62aa600fa9aeccbf3e4fc28464b4193deb462e
Facundo Ledesma <faculedesmabertalot@gmail.com>
2024-05-12 16:36:16 -0300

    Agregado PasajeroVIP

---
+++ PasajeroVIP.php
@@ -0,0 +1,41 @@
+<?php
+
+class PasajeroVIP extends Pasajero {
+
+    private $numViajeroFrecuente;
+    private $cantMillas;
+
+    public function __construct($nombre, $numAsiento, $numTicket, $numViajeroFrecuente, $cantMillas)
+    {
+        parent:: __construct($nombre, $numAsiento, $numTicket);
+        
+        $this->numViajeroFrecuente = $numViajeroFrecuente;
+        $this->cantMillas = $cantMillas;
+    }
+
+    // Getters
+    public function getNumViajeroFrecuente() {
+        return $this->numViajeroFrecuente;
+    }
+    public function getCantMillas() {
+        return $this->cantMillas;
+    }
+
+    // Setters
+    public function setNumViajeroFrecuente($numViajeroFrecuente) {
+        $this->numViajeroFrecuente = $numViajeroFrecuente;
+    }
+    public function setCantMillas($cantMillas) {
+        $this->cantMillas = $cantMillas;
+    }
+
+    // __toString
+    public function __toString()
+    {
+        $cadena = parent:: __toString();
+        $cadena .= "----------------------\nCNúmero de Viajero Frecuente: " .$this->getNumViajeroFrecuente(). 
+        $cadena .= ".\nCantidad de Millas: " .$this->getCantMillas(). ".\n";
+        return $cadena; 
+    }
+
+}
